556bf1183981a04c1ca46cc417b705c1e141d245
rashadalhashmi <86948752+rashadalhashmi@users.noreply.github.com>
2022-01-12 15:11:28 +0200

    add Identity

--- Guestbook/Data/Context.cs
+++ Guestbook/Data/Context.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace Guestbook.Data
 {
  
-    public class Context: IdentityDbContext<ApplicationUserEntity>
+    public class Context:IdentityDbContext<ApplicationUserEntity>
     {
         public Context(DbContextOptions options):base(options)
         {
