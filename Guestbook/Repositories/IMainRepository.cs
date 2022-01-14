f95b38f489fb8f35ea8ec600e0cfdd7f5da30a53
rashadalhashmi <86948752+rashadalhashmi@users.noreply.github.com>
2022-01-14 09:46:32 +0200

    add Main Repository

---
+++ Guestbook/Repositories/IMainRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Guestbook.Repositories
+{
+    public interface IMainRepository<T>
+    {
+        Task<IEnumerable<T>> Get();
+        Task<T> Get(string id);
+        Task<T> Add(T entity);
+        Task<IEnumerable<T>> AddRange(IEnumerable<T> entity);
+        Task<T> Update(T entity);
+        Task<IEnumerable<T>> Update(IEnumerable<T> entity);
+        Task<T> Delete(string id);
+        Task<IEnumerable<T>> Delete(IEnumerable<T> entity);
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ Guestbook/Repositories/MainRepository.cs
@@ -0,0 +1,67 @@
+﻿using Guestbook.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Guestbook.Repositories
+{
+  
+        //public class MainRepository<T> : IMainRepository<T> where T : BaseModel
+        //{
+        //    Context Context;
+        //    DbSet<T> Table;
+        //    public MainRepository(Context context)
+        //    {
+        //        Context = context;
+        //        Table = Context.Set<T>();
+        //    }
+        //    public async Task<IEnumerable<T>> Get()
+        //    {
+        //        return Table;
+        //    }
+        //    public async Task<T> Get(string id)
+        //    {
+        //        return await Table.Where(i => i.ID == id).AsNoTracking<T>().FirstOrDefaultAsync();
+        //    }
+        //    public async Task<T> Add(T entity)
+        //    {
+        //        await Table.AddAsync(entity);
+        //        await Context.SaveChangesAsync();
+        //        return entity;
+        //    }
+        //    public async Task<IEnumerable<T>> AddRange(IEnumerable<T> entity)
+        //    {
+        //        await Table.AddRangeAsync(entity);
+        //        await Context.SaveChangesAsync();
+        //        return entity;
+        //    }
+        //    public async Task<T> Update(T entity)
+        //    {
+        //        Context.Update(entity);
+        //        await Context.SaveChangesAsync();
+        //        return entity;
+        //    }
+        //    public async Task<IEnumerable<T>> Update(IEnumerable<T> entity)
+        //    {
+        //        Table.UpdateRange(entity);
+        //        await Context.SaveChangesAsync();
+        //        return entity;
+        //    }
+        //    public async Task<T> Delete(string id)
+        //    {
+        //        T entity = await Get(id);
+        //        Table.Remove(entity);
+        //        await Context.SaveChangesAsync();
+        //        return entity;
+        //    }
+        //    public async Task<IEnumerable<T>> Delete(IEnumerable<T> entity)
+        //    {
+        //        Table.RemoveRange(entity);
+        //        await Context.SaveChangesAsync();
+        //        return entity;
+        //    }
+
+        //    public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        //    {
+        //        return Context.Set<T>().Where(expression);
+        //    }
+        //}
+}
